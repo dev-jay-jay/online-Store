78ce0c0b3da056e0783ce37583b98af0f0a24515
dev-jay-jay <johngratia2@gmail.com>
2023-09-07 02:27:08 +0100

    deleted previous files and redisigning the online web app again

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import NavBar from "./component/nav";
-import Header from "./component/header";
-import Card from "./component/card";
-import SubTitle from "./component/subTitle";
+import './App.css';
+import Login from './component/Login';
 
 function App() {
   return (
-      <div>
-         <Header />
-        <NavBar />
-        <Card showDirection={true} />
-        <SubTitle header='Avaliable Stylish Wardrobe Essentials at JayStore' />
-        <Card showDirection={false} />
-        <SubTitle header='Explore Avaliable Top Tech Gadgets Online!' />
-        <Card showDirection={false} /> 
-      </div>
+      <>
+         <Login />
+      </>
   );
 }
 
