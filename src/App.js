3adb9aeed0cf611ca04ed5bba99ed3b2a90d0aac
dev-jay-jay <dev-jay-jay>
2023-12-19 16:14:51 +0100

    reconstruting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
- import {  Route, Routes } from 'react-router-dom';
 import './App.css';
-import Footer from './component/Card/Footer';
-import Header from './component/HeaderCompo/Header';
-import Home from './component/Card/Home';
-import Jewellery from './component/Jewellery';
-import Electronic from './component/Electronic';
-import Cloths from './component/Cloths';
-//taliwind imported you can use it now in all component
+
 
 function App() {
 
@@ -15,15 +8,7 @@ function App() {
 
   return (
       <>
-        <Header />
-        {/* Nav Links  */}
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/Cloths' element={<Cloths />} />
-          <Route path='/Electronic' element={<Electronic />} />
-          <Route path='/Jewellery' element={<Jewellery />} />
-        </Routes>
-        <Footer />
+        
       </>
   );
 }
